ce467e840dbe2723f43b51883480596fe615f2a6
Stanislav Goncharov <stas76644@icloud.com>
2022-01-29 12:39:29 +0300

    57 For loop

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
+	[TextArea(2,6)] 
 	[SerializeField] string question = "Enter new question text here";
-	[TextArea(2,6)] [SerializeField] string[] answers = new string [4];
+	[SerializeField] string[] answers = new string [4];
 	[SerializeField] int correctAnswerIndex;
 	
 	public int GetCorrectAnswerIndex()
--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,10 +7,16 @@ public class Quiz : MonoBehaviour
 {
 	[SerializeField] QuestionSO question;
 	[SerializeField] TextMeshProUGUI questionText;
+	[SerializeField] GameObject[] answerButtons;
 	
 	private void Start()
 	{
 		questionText.text = question.GetQuestion();
+		for (int i = 0; i < answerButtons.Length; i++)
+		{
+			TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+			buttonText.text = question.GetAnswer(i);
+		}
 	}
 
 }
